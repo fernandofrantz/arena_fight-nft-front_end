a7a8d1398608b1f8b54be64af8f2bdcb95d4cd12
fernandofrantz <fernandofrantz12@gmail.com>
2022-01-17 14:50:29 -0300

    resizing fighter card

--- src/components/LutadorCard/style.js
+++ src/components/LutadorCard/style.js
@@ -1,294 +1,300 @@
 import styled from "styled-components";
-import fundo from "../../assets/boxzinha.png"
+import fundo from "../../assets/boxzinha.png";
 
 export const Container = styled.div`
-    position: relative;
-    background: url(${fundo});
-    border: none;
-    background-color: Transparent;
-    background-size: cover;
-    background-position-x: center;
-    width: 90%;
-    height: 22%;
-    margin: 0 auto;
-    
-    @media screen and (min-width: 375px) {
-        height: 26%;
-    }
-    @media screen and (min-width: 425px) {
-        height: 30%;
-    }
-    @media screen and (min-width: 768px) {
-        height: 54%;
-    }
-    @media screen and (min-width: 1024px) {
-        height: 55%;
-        background-size: contain;
-        background-repeat: no-repeat;
-    }
-    @media screen and (min-width: 1920px) {
+  position: relative;
+  background: url(${fundo});
+  border: none;
+  background-color: Transparent;
+  background-size: cover;
+  background-position-x: center;
+  width: 90%;
+  min-width: 300px;
+  min-height: 200px;
+  margin: 0 auto;
 
-    }
-    @media screen and (min-width: 2560px) {
+  @media screen and (min-width: 375px) {
+    min-height: 230px;
+  }
+  @media screen and (min-width: 425px) {
+    min-height: 270px;
+  }
 
-    }
+  @media screen and (min-width: 768px) {
+    width: 60%;
+    min-height: 320px;
+    max-height: 350px;
+  }
+  @media screen and (min-width: 1024px) {
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+  @media screen and (min-width: 1920px) {
+    min-height: 450px;
+  }
+  @media screen and (min-width: 2560px) {
+  }
 `;
 
 export const Lutador = styled.img`
-    position: absolute;
-    left: 0;
-    top: 6%;
-    width: 140px;
-    @media screen and (min-width: 375px) {
-        width: 160px;
-    }
-    @media screen and (min-width: 425px) {
-        width: 180px;
-    }
-    @media screen and (min-width: 768px) {
-        width: 330px;
-    }
-    @media screen and (min-width: 1024px) {
-        left: 100px;
-    }
-    @media screen and (min-width: 1440px) {
-        width: 400px;
-        left: 240px;
-    }
-    @media screen and (min-width: 1920px) {
-        width: 450px;
-        left: 430px;
-    }
-    @media screen and (min-width: 2560px) {
-
-    }
+  position: absolute;
+  left: 0;
+  top: 6%;
+  width: 140px;
+  @media screen and (min-width: 375px) {
+    width: 160px;
+  }
+  @media screen and (min-width: 425px) {
+    width: 180px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 220px;
+  }
+  @media screen and (min-width: 1024px) {
+    width: 230px;
+    left: 90px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 230px;
+    left: 200px;
+  }
+  @media screen and (min-width: 1920px) {
+    width: 350px;
+    left: 250px;
+  }
+  @media screen and (min-width: 2560px) {
+    left: 440px;
+  }
 `;
 
 export const Xp = styled.img`
-    position: absolute;
-    width: 150px;
-    z-index: 9;
-    right: 15px;
-    top: 58px;
-    @media screen and (min-width: 375px) {
-        width: 180px;
-        top: 80px;
-    }
-    @media screen and (min-width: 425px) {
-        width: 200px;
-        top: 90px;
-    }
-    @media screen and (min-width: 768px) {
-        width: 370px;
-        top: 150px;
-        right: 20px;
-    }
-    @media screen and (min-width: 1024px) {
-        right: 140px;
-    }
-    @media screen and (min-width: 1440px) {
-        right: 260px;
-        width: 410px;
-        top: 190px;
-    }
-    @media screen and (min-width: 1920px) {
-        right: 470px;
-        width: 430px;
-        top: 220px;
-    }
-    @media screen and (min-width: 2560px) {
-
-    }
+  position: absolute;
+  width: 150px;
+  z-index: 9;
+  right: 15px;
+  top: 58px;
+  @media screen and (min-width: 375px) {
+    width: 160px;
+  }
+  @media screen and (min-width: 425px) {
+    width: 200px;
+    top: 90px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 200px;
+    top: 110px;
+    right: 50px;
+  }
+  @media screen and (min-width: 1024px) {
+    right: 110px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 180px;
+    right: 260px;
+    top: 110px;
+  }
+  @media screen and (min-width: 1920px) {
+    right: 290px;
+    width: 280px;
+    top: 180px;
+  }
+  @media screen and (min-width: 2560px) {
+    right: 480px;
+    width: 280px;
+    top: 180px;
+  }
 `;
 
 export const Thirst = styled.img`
-    position: relative;
-    width: 65px;
-    z-index: 9;
-    right: 90px;
-    bottom: 35px;
-    @media screen and (min-width: 375px) {
-        width: 77px;
-        right: 100px;
-    }
-    @media screen and (min-width: 425px) {
-        width: 90px;
-        right: 115px;
-    }
-    @media screen and (min-width: 768px) {
-        width: 180px;
-        right: 200px;
-        bottom: 55px;
-    }
-    @media screen and (min-width: 1024px) {
-        right: 300px;
-    }
-    @media screen and (min-width: 1440px) {
-        width: 210px;
-        right: 485px;
-        bottom: 65px;
-    }
-    @media screen and (min-width: 1920px) {
-        right: 39%;
-    }
-    @media screen and (min-width: 2560px) {
-
-    }
+  position: absolute;
+  width: 65px;
+  z-index: 9;
+  bottom: 35px;
+  @media screen and (min-width: 375px) {
+    width: 75px;
+  }
+  @media screen and (min-width: 425px) {
+    width: 90px;
+    right: 115px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 85px;
+    right: 150px;
+    bottom: 60px;
+  }
+  @media screen and (min-width: 1024px) {
+    right: 230px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 90px;
+    right: 350px;
+    bottom: 65px;
+  }
+  @media screen and (min-width: 1920px) {
+    width: 120px;
+    right: 39%;
+  }
+  @media screen and (min-width: 2560px) {
+    right: 42%;
+  }
 `;
 
 export const Hunger = styled.img`
-    position: absolute;
-    width: 55px;
-    z-index: 9;
-    right: 20px;
-    bottom: 35px;
-    @media screen and (min-width: 375px) {
-        width: 70px;
-    }
-    @media screen and (min-width: 425px) {
-        width: 85px;
-    }
-    @media screen and (min-width: 768px) {
-        width: 155px;
-        right: 35px;
-        bottom: 55px;
-    }
-    @media screen and (min-width: 1024px) {
-        right: 135px;
-    }
-    @media screen and (min-width: 1440px) {
-        width: 205px;
-        right: 265px;
-        bottom: 65px;
-    }
-    @media screen and (min-width: 1920px) {
-        right: 27%;
-    }
-    @media screen and (min-width: 2560px) {
-
-    }
+  position: absolute;
+  width: 55px;
+  z-index: 9;
+  right: 20px;
+  bottom: 35px;
+  @media screen and (min-width: 375px) {
+    width: 65px;
+  }
+  @media screen and (min-width: 425px) {
+    width: 85px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 75px;
+    right: 50px;
+    bottom: 60px;
+  }
+  @media screen and (min-width: 1024px) {
+    right: 135px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 80px;
+    right: 255px;
+    bottom: 65px;
+  }
+  @media screen and (min-width: 1920px) {
+    width: 105px;
+    right: 27%;
+  }
+  @media screen and (min-width: 2560px) {
+    right: 33%;
+  }
 `;
 
-
 export const Fight = styled.img`
-    width: 140px;
-    position: absolute;
-    z-index: 9;
-    bottom: -15px;
-    right: 50%;
-    left: 30%;
-    @media screen and (min-width: 768px) {
-        width: 200px;
-    }
-    @media screen and (min-width: 1440px) {
-        width: 250px;
-    }
-    @media screen and (min-width: 1920px) {
-        left: 45%;
-    }
+  width: 140px;
+  position: absolute;
+  z-index: 9;
+  bottom: -15px;
+  right: 50%;
+  left: 30%;
+  @media screen and (min-width: 375px) {
+    width: 150px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 200px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 250px;
+  }
+  @media screen and (min-width: 1920px) {
+    left: 45%;
+  }
 `;
 
 export const Lvl = styled.img`
-    width: 60px;
-    top: -25px;
-    position: absolute;
-    right: -15px;
-    @media screen and (min-width: 375px) {
-        width: 80px;
-        right: -19px;
-    }
-    @media screen and (min-width: 425px) {
-        width: 86px;
-    }
-    @media screen and (min-width: 768px) {
-        width: 140px;
-        top: -45px;
-    }
-    @media screen and (min-width: 1024px) {
-        width: 165px;
-        top: -70px;
-        right: 60px;
-    }
-    @media screen and (min-width: 1440px) {
-        width: 190px;
-        right: 180px;
-    }
-    @media screen and (min-width: 1920px) {
-        width: 210px;
-        right: 365px;
-    }
-    @media screen and (min-width: 2560px) {
-
-    }
+  width: 60px;
+  top: -25px;
+  position: absolute;
+  right: -15px;
+  @media screen and (min-width: 375px) {
+  }
+  @media screen and (min-width: 425px) {
+    width: 86px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 90px;
+    top: -45px;
+  }
+  @media screen and (min-width: 1024px) {
+    width: 90px;
+    top: -50px;
+    right: 50px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 110px;
+    right: 160px;
+  }
+  @media screen and (min-width: 1920px) {
+    width: 150px;
+    right: 220px;
+  }
+  @media screen and (min-width: 2560px) {
+    right: 380px;
+  }
 `;
 
 export const Str = styled.img`
-    position: absolute;
-    width: 120px;
-    height: 35px;
-    z-index: 9;
-    right: 45px;
-    top: 15px;
-    @media screen and (min-width: 375px) {
-        width: 130px;
-        height: 44px;
-        right: 65px;
-    }
-    @media screen and (min-width: 425px) {
-        width: 140px;
-        height: 50px;
-        right: 75px;
-        top: 22px;
-    }
-    @media screen and (min-width: 768px) {
-        width: 265px;
-        height: 80px;
-        right: 125px;
-        top: 45px;
-    }
-    @media screen and (min-width: 1024px) {
-        right: 240px;
-    }
-    @media screen and (min-width: 1440px) {
-        width: 300px;
-        height: 110px;
-        right: 375px;
-    }
-    @media screen and (min-width: 1920px) {
-        right: 600px;
-    }
-    @media screen and (min-width: 2560px) {
+  position: absolute;
+  width: 120px;
+  height: 35px;
+  z-index: 9;
+  right: 45px;
+  top: 15px;
+  @media screen and (min-width: 375px) {
+    width: 130px;
+  }
+  @media screen and (min-width: 425px) {
+    width: 140px;
+    height: 50px;
+    right: 75px;
+    top: 22px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 150px;
+    height: 50px;
+    right: 100px;
+    top: 45px;
+  }
+  @media screen and (min-width: 1024px) {
+    right: 160px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 150px;
+    height: 45px;
+    right: 290px;
+  }
 
-    }
+  @media screen and (min-width: 1920px) {
+    width: 180px;
+    height: 60px;
+    top: 100px;
+    right: 390px;
+  }
+  @media screen and (min-width: 2560px) {
+    right: 580px;
+  }
 `;
 
-
-export const Button = styled.button` 
-    border: none;
-    position: absolute;
-    z-index: 9;
-    bottom: 0;
-    right: 50%;
-    left: 30%;
-    transition: all 200ms ease-in;
-    filter: grayscale(.3) opacity(1);
-    :hover{
-        filter: grayscale(0) opacity(1);
-    }
-    ::before,::after{
-        transform: scaleY(-1);
-    }
-    @media screen and (min-width: 768px) {
-        bottom: -4px;
-        left: 34%;
-    }
-    @media screen and (min-width:1024px) {
-        left: 40%;
-    }
-    @media screen and (min-width: 1440px) {
-        bottom: -10px;
-    }
-    @media screen and (min-width: 1920px) {
-        left: 43%;
-    }
+export const Button = styled.button`
+  border: none;
+  position: absolute;
+  z-index: 9;
+  bottom: 0;
+  right: 50%;
+  left: 30%;
+  transition: all 200ms ease-in;
+  filter: grayscale(0.3) opacity(1);
+  :hover {
+    filter: grayscale(0) opacity(1);
+  }
+  ::before,
+  ::after {
+    transform: scaleY(-1);
+  }
+  @media screen and (min-width: 768px) {
+    bottom: -4px;
+    left: 34%;
+  }
+  @media screen and (min-width: 1024px) {
+    left: 40%;
+  }
+  @media screen and (min-width: 1440px) {
+    bottom: -10px;
+  }
+  @media screen and (min-width: 1920px) {
+    left: 43%;
+  }
 `;
--- src/pages/MyGym/styles.js
+++ src/pages/MyGym/styles.js
@@ -1,16 +1,17 @@
-import styled from 'styled-components';
-import background from '../../assets/gympix.png';
+import styled from "styled-components";
+import background from "../../assets/gympix.png";
 
 export const Container = styled.div`
-        margin: 0;
-        padding: 0;
-        background-image: url(${background});
-        background-repeat: no-repeat;
-        color: #fff;
-        width: 100vw;
-        height: 100vh;
-        font-family: 'Oswald', sans-serif;
-        background-color: #dc8a59;
-        background-position-x: center;
-        background-size: cover;
+  margin: 0;
+  padding: 0;
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  color: #fff;
+  width: 100vw;
+  height: 100%;
+  font-family: "Oswald", sans-serif;
+  background-color: #dc8a59;
+  background-position-x: center;
+  background-size: cover;
+  min-height: 100vh;
 `;
